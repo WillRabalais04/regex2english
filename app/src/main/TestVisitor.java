4dfee40e7c3e20b33b6c6b9103ad5db47a112ccb
Will Rabalais <69363495+WillRabalais04@users.noreply.github.com>
2024-02-24 13:36:47 -0500

    finally fixed project structure so generated classes can be accessed in the right package

--- app/src/main/TestVisitor.java
+++ app/src/main/TestVisitor.java
@@ -2,8 +2,6 @@ package app.src.main;
 
 public class TestVisitor extends regex2englishBaseVisitor<String>{
 
-
-
     @Override
     public String visitStart(regex2englishParser.StartContext ctx){
 
